c2177f3447ce422008b848f34359efdb82d8ef76
Samer <smoor.samara1999@gmail.com>
2024-04-06 14:07:58 +0200

    adding contact componant

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter , Routes , Route } from "react-router-dom";
 import MapMarker from "./Components/MapMarker";
 import Footer from "./Components/Footer";
 import GarageOwner from "./Pages/MainLogin/GarageOwner";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
   return (
@@ -23,9 +24,9 @@ function App() {
         <Route path="/signup" element ={<Signup/>} />
         <Route path="/service" element ={<Service/>} />
         <Route path="/Garageowner" element ={<GarageOwner/>} />
+        <Route path="/contact" element ={<Contact/>} />
 
       </Routes>
-      {/* <GarageOwner/> */}
       </BrowserRouter>
       
     
---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navgation from '../../Components/Navbar/Navgation'
+function Contact() {
+  return (
+    <div>
+        <Navgation/>
+        <h1>welcome</h1>
+    </div>
+  )
+}
+
+export default Contact
