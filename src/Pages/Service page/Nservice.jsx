3060d89cd2f0cc5a8adf7b4a87fbac5d03cd587f
omar <oyomaryasin@gmail.com>
2024-04-09 17:54:13 +0300

    added the directions to map

--- src/Pages/Service page/Nservice.jsx
+++ src/Pages/Service page/Nservice.jsx
@@ -11,14 +11,50 @@ import NavBar from "./components/NavBar.tsx";
 import RentalCard from "./components/RentalCard.tsx";
 import HeaderSection from "./components/HeaderSection.tsx";
 import park from "./park.json";
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions"; // Modified import
 
 export default function RentalDashboard() {
   const mapRef = useRef(null);
+
+  const directionsRef = useRef(null);
   const [selectedPark, setSelectedPark] = useState(null);
 
   useEffect(() => {
     console.log(selectedPark?.geometry.coordinates);
   }, [selectedPark]);
+
+  useEffect(() => {
+    if (mapRef.current) {
+      const map = mapRef.current.getMap();
+      directionsRef.current = new MapboxDirections({
+        accessToken:
+          "pk.eyJ1Ijoib21hcmFkd24iLCJhIjoiY2x1MWE0dHE4MGJtZTJqbW5mZ3p0M3BjdyJ9.TgSibF8OzyeO3mgVef1wNw",
+        unit: "metric",
+        profile: "mapbox/driving",
+        controls: { instructions: true },
+      });
+      map.addControl(directionsRef.current, "top-left");
+      //this i will edit it later here should be the user location
+      directionsRef.current.setOrigin([35.29817997108734, 32.46248446267177]);
+    }
+  }, [mapRef.current]);
+  useEffect(() => {
+    return () => {
+      if (directionsRef.current) {
+        const map = mapRef.current.getMap();
+        map.removeControl(directionsRef.current);
+        directionsRef.current = null;
+      }
+    };
+  }, []);
+
+  const handlePopupClick = useCallback((parking) => {
+    const coordinates = parking.geometry.coordinates;
+    if (directionsRef.current) {
+      // If origin (point A) is already set, set the destination (point B)
+      directionsRef.current.setDestination([coordinates[0], coordinates[1]]);
+    }
+  }, []);
 
   const handleMapTransition = useCallback(({ longitude, latitude }) => {
     mapRef.current?.flyTo({
@@ -27,6 +63,7 @@ export default function RentalDashboard() {
       zoom: 17,
     });
   }, []);
+
   return (
     <CssVarsProvider disableTransitionOnChange>
       <CssBaseline />
@@ -34,7 +71,7 @@ export default function RentalDashboard() {
       <Box
         component="main"
         sx={{
-          height: "100vh", // 55px is the height of the NavBar
+          height: "100vh",
           display: "grid",
           gridTemplateColumns: { xs: "auto", md: "50% 50%" },
           gridTemplateRows: "auto 1fr auto",
@@ -65,6 +102,7 @@ export default function RentalDashboard() {
                   longitude: parking.geometry.coordinates[0],
                 })
               }
+              onPopupClick={() => handlePopupClick(parking)}
             />
           ))}
         </Stack>
@@ -81,7 +119,6 @@ export default function RentalDashboard() {
               mapboxAccessToken="pk.eyJ1Ijoib21hcmFkd24iLCJhIjoiY2x1MWE0dHE4MGJtZTJqbW5mZ3p0M3BjdyJ9.TgSibF8OzyeO3mgVef1wNw"
               style={{
                 overflow: "hidden",
-
                 width: "100%",
                 height: "100vh",
               }}
@@ -111,8 +148,9 @@ export default function RentalDashboard() {
                   longitude={selectedPark.geometry.coordinates[0]}
                   closeButton={true}
                   closeOnClick={false}
+                  onClick={() => handlePopupClick(selectedPark)}
                 >
-                  {/* <KeepMountedModal parking={selectedPark} /> */}
+                  {/* Add Popup content here */}
                 </Popup>
               )}
             </Map>
